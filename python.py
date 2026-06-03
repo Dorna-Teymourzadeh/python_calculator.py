3705a87ab6b74726ade16bf20d82e9a7b38577ed
Dorna-t-h <dorna.teymourzade2001@gmail.com>
2026-06-03 23:29:28 +0200

    Add files via upload

--- python.py
+++ python.py
@@ -14,3 +14,21 @@ print (savings)
 monthly_savings= 10
 num_months=4
 new_savings= monthly_savings*num_months
+
+half=0.5
+intro= 'Hello'
+is_good= True
+print(half)
+print(intro)
+print(is_good)
+
+saving=100
+new_saving=40
+total_saving=saving+new_saving
+print (total_saving)
+print (type (total_saving))
+
+
+intro='Hello, How are you?'
+doubleintro=intro+intro
+print(doubleintro)
