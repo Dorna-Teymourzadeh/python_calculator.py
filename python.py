83413c61e2d5e2d0f65b3e34adc0dc460f2c4353
Dorna-t-h <dorna.teymourzade2001@gmail.com>
2026-06-03 23:51:52 +0200

    Add files via upload

--- python.py
+++ python.py
@@ -32,3 +32,27 @@ print (type (total_saving))
 intro='Hello, How are you?'
 doubleintro=intro+intro
 print(doubleintro)
+
+hall=12
+kitch=11.2
+liv=15
+bed=9.6
+fam=[hall,kitch,liv,bed]
+print(fam)
+
+
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# House information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ['bedroom', bed],
+         ['bathroom',bath]]
+
+# Print out house
+print(house)
