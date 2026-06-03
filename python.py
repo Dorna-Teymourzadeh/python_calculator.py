3565679543d1ddf80aab4109b3c4efe03266cf36
Dorna-t-h <dorna.teymourzade2001@gmail.com>
2026-06-03 23:09:25 +0200

    Add files via upload

--- python.py
+++ python.py
@@ -2,3 +2,15 @@ print (4+5)
 print (5-3)
 print (4*2)
 print (3/2)
+
+
+name = 'dorna'
+print (type)
+print (name)
+
+savings= 100
+print (savings)
+
+monthly_savings= 10
+num_months=4
+new_savings= monthly_savings*num_months
